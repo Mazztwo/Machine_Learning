650e1d927d963a2f94137695357c20a7fdbff772
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-09-07 15:12:07 -0400

    Added preliminary HW2 folder and file.

---
+++ HW2/my_kmeans.m
@@ -0,0 +1,15 @@
+
+
+
+function [ids, means, ssd] = my_kmeans(A, K, iters)
+
+% 1. First, initialize the cluster means randomly. 
+% Get the range of the feature space, separately for each 
+% feature dimension (compute max and min and take the difference) 
+% and use this to request random numbers in that range. Check 
+% the documentation for rand.
+
+
+
+end
+
