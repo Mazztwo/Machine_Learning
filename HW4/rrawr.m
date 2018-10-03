9fec8e4ecf3e145d2b980ffc79a9c3853ff33ba9
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-10-03 10:58:14 -0400

    Re-did weighted-knn as I was calculating class weight wrong.

--- HW4/rrawr.m
+++ HW4/rrawr.m
@@ -7,5 +7,15 @@ X_test = [1 2 2 6; 5 2 7 3; 1 9 3 3];
 k = 2;
 sigma = 3;
 
-[y_test] = my_knn(X_train,y_train,X_test,k)
+%[y_test] = my_knn(X_train,y_train,X_test,k)
 [z_test] = weighted_knn(X_train,y_train,X_test, sigma)
+
+A = [12 36 91; 45 29 48; 33 25 11];
+A(1);
+A(2);
+A(3);
+A(4);
+
+B = [1;1;1;1;0;0;0;0;1;0;1;0;0;];
+
+C = unique(B);
