f4dcd1e1a1f8b81f15571101454582fa9a3f1b38
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-12-04 12:12:28 -0500

    Completed 1 and 2.

---
+++ HW9/naive_solution.m
@@ -0,0 +1,26 @@
+
+% Inputs:
+% the transition matrix A (from hmm_starter.m),
+% the observation matrix B (from hmm_starter.m),
+% N, the number of states,
+% M, the number of words in the vocabulary, and
+% a vector of integers sent representing the sentence whose observation probability we want to compute.
+% 
+% Outputs:
+% prob, the probability of observing the input sentence.
+
+
+
+function [prob] = naive_solution(A,B,N,M,sent)
+    
+    % Given a sentance = [X X X ... X]
+    % X = state = word
+    % We can easily find each word in B and get the probabilities if in
+    % each state
+    
+
+
+   
+end
+
+
