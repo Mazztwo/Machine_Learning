4c8ef3b4159652f8db76748900f783a5a2ec0519
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-12-10 18:11:07 -0500

    Finished naive solution. Testing now.

--- HW9/naive_solution.m
+++ HW9/naive_solution.m
@@ -4,7 +4,7 @@
 % the observation matrix B (from hmm_starter.m),
 % N, the number of states,
 % M, the number of words in the vocabulary, and
-% a vector of integers sent representing the sentence whose observation probability we want to compute.
+% sent, a vector of integers representing the sentence whose observation probability we want to compute.
 % 
 % Outputs:
 % prob, the probability of observing the input sentence.
@@ -12,15 +12,62 @@
 
 
 function [prob] = naive_solution(A,B,N,M,sent)
-    
-    % Given a sentance = [X X X ... X]
-    % X = state = word
-    % We can easily find each word in B and get the probabilities if in
-    % each state
-    
 
-
-   
-end
+    % Prob is a running sum
+    prob = 0;
+    
+    % NOT CONSIDERING TRANSITION TO END STATE 
+    % The number of words in the sentence + the start state.
+    num_states = size(sent,2) + 1;
+    
+    % Get all possible sequences of len of sentence
+    sequences = permn(A, num_states);
+    
+    % Consider all possible state sequences of length of sentence sent
+    for s = sequences
+        
+        % Compute probability of a given state sequence from A
+        state_transition_probability = 1;
+        for row = 1:num_states
+            % Can do this because I know that none of my sentences are less
+            % than two words. Bad practice otherwise!
+            col = 2;
+            
+            start_state = s(row);
+            end_state = s(col);
+            
+            state_transition_probability = state_transition_probability * A(start_state, end_state);
+            
+            col = col + 1;
+            
+            if ( col > num_states )
+                break
+            end
+        end
+        
+        % Compute probability of generating sent in each of the
+        % corresponding states in the sequence. Skip first state of state
+        % sequence as this corresponds to start state.
+        observation_probability = 1;
+        for r = 1:num_states-1
+            state = s(r+1);
+            
+            % Skip state 1
+            if ( state == 1)
+                continue
+            end
+           
+            word = sent(r);
+            
+            observation_probability = observation_probability * B (state, word);
+        end
+        
+        % Add probability of seeing sent given the state sequence to prob
+        prob_temp = state_transition_probability * observation_probability;
+        prob = prob + prob_temp;    
+        
+    end
+  
+end 
 
 
